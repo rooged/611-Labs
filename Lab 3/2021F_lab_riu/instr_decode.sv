b4f7169a967c4022dc171fc8174161cbec355cb5
Timothy Gedney <69685314+rooged@users.noreply.github.com>
2021-10-20 16:11:56 -0400

    Update instr_decode.sv

--- Lab 3/2021F_lab_riu/instr_decode.sv
+++ Lab 3/2021F_lab_riu/instr_decode.sv
@@ -1,5 +1,10 @@
 module instr_decode (input logic [31:0] in, output logic [31:0] out);
-//add
+logic [6:0] opcode;
+assign opcode = in[6:0];
+
+always_comb begin
+	if (opcode === 7'b0110011 | opcode === 7'b1110011) begin
+	//add
 //sub
 //and
 //or
@@ -13,15 +18,21 @@ module instr_decode (input logic [31:0] in, output logic [31:0] out);
 //mulh
 //mulhu
 //csrrw
-//addi
-//andi
-//ori
-//xori
-//slli
-//srai
-//srli
-//lui
-always_comb begin
+	end else if (opcode === 7'b0010011) begin
+	//addi
+	//andi
+	//ori
+	//xori
+	//slli
+	//srai
+	//srli
+	end else if (opcode === 7'b0110111) begin
+		//lui
+	end
+end
+
+
+
 if(in[6:0] == "7'b0110011")
 
 
