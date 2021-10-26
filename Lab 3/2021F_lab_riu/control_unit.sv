b553a00c2c9edd86c7d928e3d4b1c90e01532a60
Timothy Gedney <roo_ged65@yahoo.com>
2021-10-26 13:52:02 -0400

    fajga;vk

--- Lab 3/2021F_lab_riu/control_unit.sv
+++ Lab 3/2021F_lab_riu/control_unit.sv
@@ -1,10 +1,13 @@
-module control_unit (input logic [2:0] itype, //instruction type (r, i, u)
+module instr_decode (input logic [2:0] itype //instruction type (r, i, u)
 		     input logic [3:0] instr, //instruction
-			 output logic [4:0] alusrc,
+			 output logic [0:0] alusrc,
 			 output logic [0:0] regwrite, //register file write enable
-			 output logic [4:0] regsel,
+			 output logic [1:0] regsel,
 			 output logic [3:0] aluop,
 			 output logic [0:0] gpio_we); //enable writing to GPIO register
+	logic [31:0] gpio_out; //output via GPIO, gpio_we signal is the write enable for this register
+	logic [31:0] PC; //program counter
+	
 	aluop = instr;
 	alusrc = 1'bx;
 	reqwrite = 1'bx;
@@ -12,31 +15,34 @@ module control_unit (input logic [2:0] itype, //instruction type (r, i, u)
 	gpio_we = 1'bx;
 
 	always_comb begin
-	
-		if(itype == 3'b000) begin //r-types: add, sub, and, or, xor, sll, sra,
-					// srl, slt, sltu, mul, mulh, mulhu, csrrw
-
-			alusrc = 1'b0; //add, sub, mul, slt, and, sll, 
-			regsel = 2'b10;
+		//r-types: add, sub, and, or, xor, sll, sra, srl, slt, sltu, mul, mulh, mulhu, csrrw
+		if(itype == 3'b000) begin
+			if (instr == 4'b1101) begin //csrrw
+				aluop = 4'bx;
+				alusrc = 1'bx;
+				regsel = 4'bx;
+				reqwrite = 1'b0;
+				gpio_we = 1'b1;
+			end else begin
+				alusrc = 1'b0; //add, sub, mul, slt, and, sll, 
+				regsel = 2'b10;
+				regwrite = 1'b1;
+				gpio_we = 1'b0;
+			end
+		end else if (itype == 3'b001) begin
+			alusrc = 1'b1;
 			regwrite = 1'b1;
+			regsel = 2'b10;
 			gpio_we = 1'b0;
-			if(instr == 4'b1101) begin //csrrw
-			aluop = 4'bx;
+		end else if (istr == 3'b010) begin
 			alusrc = 1'bx;
-			regsel = 4'bx;
-			reqwrite = 1'b0;
-			gpio_we = 1'b1;
-			end else begin
+			regwrite = 1'b1;
+			regsel = 2'b01;
+			gpio_we = 1'b0;
+		end/* else if (istr == 3'b011) begin
 		
-
-		if(instr == 4'b1101) begin //sub
-
-			end else begin
-
-		if(instr == 4'b1101) begin //mul
-
-			end else begin
-			
+		end else if (istr == 3'b100) begin
 		
+		end*/
 	end
 endmodule
--- Lab 3/2021F_lab_riu/instr_decode.sv
+++ Lab 3/2021F_lab_riu/instr_decode.sv
@@ -28,24 +28,24 @@ module instr_decode (input logic [31:0] in, //input
 		if (opcode == 7'b0110011 | opcode == 7'b1110011) begin
 			itype = 3'b000;
 			case (funct3)
-				3'b000: instr = funct7[0] ? 4'b1010 : (funct7[5] ? 4'b0001 : 4'b0000); //add = 4'b0000, sub = 4'b0001, mul = 4'b1010
-				3'b001: instr = opcode[6] ? 4'b1101 : (funct7[0] ? 4'b1011 : 4'b0101); //sll = 4'b0101, mulh = 4'b1011, csrrw = 4'b1101
-				3'b010: instr = 4'b1000; //slt = 4'b1000
-				3'b011: instr = funct7[0] ? 4'b1100 : 4'b1001; //sltu = 4'b1001, mulhu = 4'b1100
-				3'b100: instr = 4'b0100; //xor = 4'b0100
-				3'b101: instr = funct7[5] ? 4'b0110 : 4'b0111; //sra = 4'b0110, srl = 4'b0111
-				3'b110: instr = 4'b0011; //or = 4'b0011
-				3'b111: instr = 4'b0010; //and = 4'b0010
+				3'b000: instr = funct7[0] ? 4'b0101 : (funct7[5] ? 4'b0100 : 4'b0011); //add = 4'b0011, sub = 4'b0100, mul = 4'b0101
+				3'b001: instr = opcode[6] ? 4'b1101 : (funct7[0] ? 4'b0110 : 4'b1000); //sll = 4'b1000, mulh = 4'b0110, csrrw = 4'b1101
+				3'b010: instr = 4'b1010; //slt = 4'b1010
+				3'b011: instr = funct7[0] ? 4'b0111 : 4'b1100; //sltu = 4'b1100, mulhu = 4'b0111
+				3'b100: instr = 4'b0010; //xor = 4'b0010
+				3'b101: instr = funct7[5] ? 4'b1011 : 4'b1001; //sra = 4'b1011, srl = 4'b1001
+				3'b110: instr = 4'b0001; //or = 4'b0001
+				3'b111: instr = 4'b0000; //and = 4'b0000
 		//i-type: addi, andi, ori, xori, slli, srai, srli
 		end else if (opcode == 7'b0010011) begin
 			itype = 3'b001;
 			case (funct3)
-				3'b000: instr = 4'b0000; //addi = 4'b0000
-				3'b001: instr = 4'b0100; //slli = 4'b0100
-				3'b100: instr = 4'b0011; //xori = 4'b0011
-				3'b101: instr = funct7[5] ? 4'b0101 : 4'b0110; //srai = 4'b0101, srli = 4'b0110
-				3'b110: instr = 4'b0010; //ori = 4'b0010
-				3'b111: instr = 4'b0001; //andi = 4'b0001
+				3'b000: instr = 4'b0011; //addi = 4'b0011
+				3'b001: instr = 4'b1000; //slli = 4'b1000
+				3'b100: instr = 4'b0010; //xori = 4'b0010
+				3'b101: instr = funct7[5] ? 4'b1100 : 4'b1001; //srai = 4'b1100, srli = 4'b1001
+				3'b110: instr = 4'b0001; //ori = 4'b0001
+				3'b111: instr = 4'b0000; //andi = 4'b0000
 		//u-type: lui
 		end else if (opcode == 7'b0110111) begin
 			itype = 3'b010;
