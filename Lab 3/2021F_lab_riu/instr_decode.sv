953f8538974362ae048c1fe3f70af3efdb305d5c
Timothy Gedney <69685314+rooged@users.noreply.github.com>
2021-11-04 13:05:06 -0400

    Update instr_decode.sv

--- Lab 3/2021F_lab_riu/instr_decode.sv
+++ Lab 3/2021F_lab_riu/instr_decode.sv
@@ -24,34 +24,36 @@ module instr_decode (input logic [31:0] in, //input
 	assign shamt = in[24:20];
 
 	always_comb begin
+		itype <= 3'b000;
+		instr <= 4'b0000; 
 		//r-types: add, sub, and, or, xor, sll, sra, srl, slt, sltu, mul, mulh, mulhu, csrrw
 		if (opcode == 7'b0110011 | opcode == 7'b1110011) begin
-			itype = 3'b000;
+			itype <= 3'b000;
 			case (funct3)
-				3'b000: instr = funct7[0] ? 4'b0101 : (funct7[5] ? 4'b0100 : 4'b0011); //add = 4'b0011, sub = 4'b0100, mul = 4'b0101
-				3'b001: instr = opcode[6] ? 4'b1101 : (funct7[0] ? 4'b0110 : 4'b1000); //sll = 4'b1000, mulh = 4'b0110, csrrw = 4'b1101
-				3'b010: instr = 4'b1010; //slt = 4'b1010
-				3'b011: instr = funct7[0] ? 4'b0111 : 4'b1100; //sltu = 4'b1100, mulhu = 4'b0111
-				3'b100: instr = 4'b0010; //xor = 4'b0010
-				3'b101: instr = funct7[5] ? 4'b1011 : 4'b1001; //sra = 4'b1011, srl = 4'b1001
-				3'b110: instr = 4'b0001; //or = 4'b0001
-				3'b111: instr = 4'b0000; //and = 4'b0000
+				3'b000: instr <= funct7[0] ? 4'b0101 : (funct7[5] ? 4'b0100 : 4'b0011); //add = 4'b0011, sub = 4'b0100, mul = 4'b0101
+				3'b001: instr <= opcode[6] ? 4'b1101 : (funct7[0] ? 4'b0110 : 4'b1000); //sll = 4'b1000, mulh = 4'b0110, csrrw = 4'b1101
+				3'b010: instr <= 4'b1010; //slt = 4'b1010
+				3'b011: instr <= funct7[0] ? 4'b0111 : 4'b1100; //sltu = 4'b1100, mulhu = 4'b0111
+				3'b100: instr <= 4'b0010; //xor = 4'b0010
+				3'b101: instr <= funct7[5] ? 4'b1011 : 4'b1001; //sra = 4'b1011, srl = 4'b1001
+				3'b110: instr <= 4'b0001; //or = 4'b0001
+				3'b111: instr <= 4'b0000; //and = 4'b0000
 			endcase
 		//i-type: addi, andi, ori, xori, slli, srai, srli
 		end else if (opcode == 7'b0010011) begin
-			itype = 3'b001;
+			itype <= 3'b001;
 			case (funct3)
-				3'b000: instr = 4'b0011; //addi = 4'b0011
-				3'b001: instr = 4'b1000; //slli = 4'b1000
-				3'b100: instr = 4'b0010; //xori = 4'b0010
-				3'b101: instr = funct7[5] ? 4'b1100 : 4'b1001; //srai = 4'b1100, srli = 4'b1001
-				3'b110: instr = 4'b0001; //ori = 4'b0001
-				3'b111: instr = 4'b0000; //andi = 4'b0000
+				3'b000: instr <= 4'b0011; //addi = 4'b0011
+				3'b001: instr <= 4'b1000; //slli = 4'b1000
+				3'b100: instr <= 4'b0010; //xori = 4'b0010
+				3'b101: instr <= funct7[5] ? 4'b1100 : 4'b1001; //srai = 4'b1100, srli = 4'b1001
+				3'b110: instr <= 4'b0001; //ori = 4'b0001
+				3'b111: instr <= 4'b0000; //andi = 4'b0000
 			endcase
 		//u-type: lui
 		end else if (opcode == 7'b0110111) begin
-			itype = 3'b010;
-			instr = 4'b0000; //lui = 4'b0000
+			itype <= 3'b010;
+			instr <= 4'b0000; //lui = 4'b0000
 		end/* else if (opcode === 7'b"B-type opcode") begin
 			assign itype = 3'b011;
 		end else if (opcode === 7'b"J-type opcode") begin
