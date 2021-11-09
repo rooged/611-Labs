35e1fdc62d8a91dc759c73000d7b7ae2329e5727
Timothy Gedney <roo_ged65@yahoo.com>
2021-11-09 14:53:30 -0500

    bin2dec working!!

--- Lab 3/2021F_lab_riu/bin2dec.asm
+++ Lab 3/2021F_lab_riu/bin2dec.asm
@@ -1,10 +1,10 @@
-#csrrw t2, 0xf00, x9
+csrrw t2, 0xf00, x1
 
 lui t1, 104857
 addi t1, t1, 2047 # 0.1 in 32,32 constant
 addi t1, t1, 411
 
-addi t2, zero, 1852 # 32,0 FP dummy variable 1468
+addi t2, zero, 1854 # 32,0 FP dummy variable 1468
 addi t6, zero, 10 # constant 10
 
 
@@ -12,91 +12,65 @@ addi t6, zero, 10 # constant 10
 # t1 : constant 0.1
 # t2: input val dummy variable
 # t6: constant 10
-# s1: hex0
-# s2: hex1
-# s3: hex2
-# s4: hex3
-# s5: hex4
-# s6: hex5
-# s7: hex6
-# s8: hex7
-#
+# s1: mulhu final register
+# s2: output reg
 
 # HEX0 
 mulhu  t3, t2, t1 # 0.1 x 1468 = 146
 mul t4, t2, t1 # 0.1 x 1468 = .8
 mulhu s1, t4, t6 # hex0 = .8 * 10 = 8
-add s10, zero, s1
-#add s10, zer
-#srli s10, s9, 4
-#csrrw x1, 0xf02,s1
+add s2, zero, s1
 
 # HEX1
 mulhu t5, t3, t1 # 0.1 x 146 = 14
 mul t4, t3, t1 # 0.1 x 146 = .6
-mulhu s2, t4, t6 # hex1 = .6 x 10 = 6
-slli s9, s9, 4
-or s10, s9, s10
-#add s9, zero, s2
-#srli s10, s9, 4
-
-#csrrw x2, 0xf02,s2
+mulhu s1, t4, t6 # hex1 = .6 x 10 = 6
+slli s1, s1, 4
+or s2, s1, s2
 
 # HEX2
 mulhu t2, t5, t1 # 0.1 x 14 = 1
 mul a1, t5, t1 # 0.1 x 14 = .4
-mulhu s3, a1, t6 # .4 x 10 = 4
-add s9, zero, s3
-srli s10, s9, 4
-#csrrw x3, 0xf02,s3
+mulhu s1, a1, t6 # .4 x 10 = 4
+slli s1, s1, 8
+or s2, s1, s2
 
 # HEX3
 mulhu t3, t2, t1 # 0.1 x 1 = 0
 mul a2, t2, t1 # 0.1 x 1 = .1
-mulhu s4, a2, t6 # 0.1 x 10 = 1
-add s9, zero, s4
-srli s10, s9, 4
-#csrrw x4, 0xf02,s4
+mulhu s1, a2, t6 # 0.1 x 10 = 1
+slli s1, s1, 12
+or s2, s1, s2
 
 # HEX4
 mulhu t5, t3, t1 # 0.1 x 0 = 0
 mul a1, t3, t1 # 0.1 x 0 = 0
-mulhu s5, a1, t6 # 0 x 10 = 0
-add s9, zero, s5
-srli s10, s9, 4
-#csrrw x5, 0xf02,s5
+mulhu s1, a1, t6 # 0 x 10 = 0
+slli s1, s1, 16
+or s2, s1, s2
 
 # HEX5
 mulhu t4, t5, t1 # 0.1 x 0 = 0
 mul t3, t5, t1 # 0.1 x 0 = 0
-mulhu s6, t3, t6 # 0 x 10 = 0
-add s9, zero, s6
-srli s10, s9, 4
-#csrrw x6, 0xf02,s6
+mulhu s1, t3, t6 # 0 x 10 = 0
+slli s1, s1, 20
+or s2, s1, s2
 
 # HEX6
 mulhu t2, t4, t1 # 0.1 x 0 = 0
 mul t5, t4, t1 # 0.1 x 0 = 0
-mulhu s7, t5, t6 # 0 x 10 = 0
-add s9, zero, s7
-slli s9, s9, 4
-or s10, s9, s10
-#srli s10, s9, 4
-#csrrw x7, 0xf02,s7
+mulhu s1, t5, t6 # 0 x 10 = 0
+slli s1, s1, 24
+or s2, s1, s2
 
 # HEX7
 mulhu t3, t2, t1 # 0.1 x 0 = 0
 mul a1, t2, t1 # 0.1 x 0 = 0
-mulhu s8, a1, t6 # 0 x 10 = 0
-add s9, zero, s8
-srli s10, s9, 4
+mulhu s1, a1, t6 # 0 x 10 = 0
+slli s1, s1, 28
+or s2, s1, s2
 
-slli s11, s10, 4
-ori s10, s11, 0
-
-
-
-#csrrw x8, 0xf02,s8
+csrrw x2, 0xf02, s2
 
 
 
