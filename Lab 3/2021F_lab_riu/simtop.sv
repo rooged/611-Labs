65655c314a7b03a880b42c18d385a4c237ddeac5
Timothy Gedney <69685314+rooged@users.noreply.github.com>
2021-11-10 17:41:04 -0500

    Update simtop.sv

--- Lab 3/2021F_lab_riu/simtop.sv
+++ Lab 3/2021F_lab_riu/simtop.sv
@@ -38,15 +38,53 @@ module simtop;
 		.HEX6(HEX6),
 		.HEX7(HEX7)
 	);
-
 	//self checking testbench
-	/*initial begin
-		//checks first display at 0
-		SW <= 18'b000000000000000000;
-		#10;
-		if (HEX0 !== 7'b1000000) begin
-			$error("HEX0 %x was not 7'b1000000", HEX0);
-		end
-	end*/
-endmodule
+	always begin
+		clk = 0;
+		#5;
+		clk = 1;
+		#5;
+	end
 
+	initial begin
+		SW <= 18'd12345;
+		rst <= 0;
+		#10;
+		rst <= 1;
+		#500
+		if (HEX7 != 7'h40 || HEX6 != 7'h40 || HEX5 != 7'h40 || HEX4 != 7'h79 ||
+			HEX3 != 7'h24 || HEX2 != 7'h30 || HEX1 != 7'h19 || HEX0 != 7'h12) begin
+			$error("Test 1 Failed");
+		end
+
+		SW <= 18'd6789;
+		rst <= 0;
+		#10;
+		rst <= 1;
+		#500
+		if (HEX7 != 7'h40 || HEX6 != 7'h40 || HEX5 != 7'h40 || HEX4 != 7'h40 ||
+			HEX3 != 7'h02 || HEX2 != 7'h78 || HEX1 != 7'h00 || HEX0 != 7'h18) begin
+			$error("Test 2 Failed");
+		end
+
+		SW <= 18'd262143;
+		rst <= 0;
+		#10;
+		rst <= 1;
+		#500
+		if (HEX7 != 7'h40 || HEX6 != 7'h40 || HEX5 != 7'h24 || HEX4 != 7'h02 ||
+			HEX3 != 7'h24 || HEX2 != 7'h79 || HEX1 != 7'h19 || HEX0 != 7'h30) begin
+			$error("Test 3 Failed");
+		end
+
+		SW <= 18'd0897;
+		rst <= 0;
+		#10;
+		rst <= 1;
+		#500
+		if (HEX7 != 7'h40 || HEX6 != 7'h40 || HEX5 != 7'h40 || HEX4 != 7'h40 ||
+			HEX3 != 7'h40 || HEX2 != 7'h00 || HEX1 != 7'h18 || HEX0 != 7'h78) begin
+			$error("Test 4 Failed");
+		end
+	end
+endmodule
